ec535ca4edf1ec3d66de2e5fd0c3f15d2f7ad347
Joonatan Kallio <kallio.joonatan1@gmail.com>
2023-02-15 21:10:02 +0200

    Initial commit

---
+++ server/app.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+const mongoose = require('mongoose');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+const MongoDB = "mongodb://localhost:27017/projectdb"
+mongoose.connect(MongoDB);
+mongoose.Promise = Promise;
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "MongoDB connection error"))
+
+module.exports = app;
