2b8dc4ccb2b4dc1562595b119d31d103ab81c6bc
Joonatan Kallio <kallio.joonatan1@gmail.com>
2023-02-15 21:15:36 +0200

    Added CORS

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require("cors")
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -17,6 +18,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+if(process.env.NODE_ENV === "development") {
+    var corsOptions = {
+        origin: 'http://localhost:3000/',
+        optionsSuccessStatus:200,
+    };
+    app.use(cors(corsOptions));
+}
 
 const MongoDB = "mongodb://localhost:27017/projectdb"
 mongoose.connect(MongoDB);
